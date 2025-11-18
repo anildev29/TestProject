9dcf73325596bb15a453c6c995df7bf203ca32d2
adambasha12 <adambashas111@gmail.com>
2025-11-18 10:34:44 +0530

    @JavaIntroduction File has added@

---
+++ javaIntroduction.java
@@ -0,0 +1,11 @@
+00:00 - Introduction
+00:12 - What Is Java & Why Should I Learn Java?
+19:32 - History of Java
+39:02 - Interview questions - part1
+44:36 - Difference between Program, Application, Library & Package
+59:48 - Steps to execute a java program
+01:25:42 - What is Platform Independence & How Java achieves it?
+01:55:32 - What is JVM (Java Virutal Machine)
+02:25:11 - Install Java
+02:54:42 - Write Your First Java Application
+03:54:19 - Interview questions - Part 2
